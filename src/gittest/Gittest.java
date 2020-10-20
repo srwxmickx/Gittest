ff13d3b1288728da8a4d696b24a18d76b9343cc0
Surawit <surawit.mick@mail.kmutt.ac.th>
2020-10-20 16:54:07 +0700

    Gittest

---
+++ src/gittest/Gittest.java
@@ -0,0 +1,13 @@
+package gittest;
+
+
+public class Gittest {
+
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+}
